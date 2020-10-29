5467babb88712dc8f37a2e8060f1eb500e4380b0
Aliaksandr-Strr <58819659+Aliaksandr-Strr@users.noreply.github.com>
2020-10-29 16:06:22 +0300

    Update tui_offices.py

--- tui_offices.py
+++ tui_offices.py
@@ -1,49 +1,35 @@
 import json
-import requests
-from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 
 
 class Parser:
 
     def __init__(self):
+        self.session = HTMLSession()
         self.url = 'https://apigate.tui.ru/api/office/list?cityId=1&subwayId=&hoursFrom=&hoursTo=&serviceIds=all&toBeOpenOnHolidays=false'
-        self.headers = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36 OPR/70.0.3728.189'
-        }
         self.offices = []
         self.json = 'offices.json'
 
-    def get_json(self):
-        try:
-            result = requests.get(self.url, headers=self.headers)
-            return result.json()
-
-        except Exception as ex:
-            print(ex)
-
     def processing_data(self):
-        all_data = self.get_json()
+        result = self.session.get(url=self.url)
+        all_data = result.json()
         for data in all_data['offices']:
-            weekday = f"Пн-Пт {data['hoursOfOperation']['workdays']['startStr']}-" \
-                      f"{data['hoursOfOperation']['workdays']['endStr']}"
-            if not data['hoursOfOperation']['saturday']['isDayOff']:
-                saturday = f"Сб {data['hoursOfOperation']['saturday']['startStr']}-" \
-                           f"{data['hoursOfOperation']['saturday']['endStr']}"
-            else:
-                saturday = 'Сб выходной'
-            if not data['hoursOfOperation']['sunday']['isDayOff']:
-                sunday = f"Вс {data['hoursOfOperation']['sunday']['startStr']}-" \
-                         f"{data['hoursOfOperation']['sunday']['endStr']}"
-            else:
-                sunday = 'Вс выходной'
+            working_hours = []
+            workdays_hours = data['hoursOfOperation']['workdays']
+            saturday_hours = data['hoursOfOperation']['saturday']
+            sunday_hours = data['hoursOfOperation']['sunday']
+            working_hours.append(f"Пн-Пт {workdays_hours.get('startStr')} - {workdays_hours.get('endStr')}")
+            if not saturday_hours['isDayOff']:
+                working_hours.append(f"Сб {saturday_hours.get('startStr')} - {saturday_hours.get('endStr')}")
+            if not sunday_hours['isDayOff']:
+                working_hours.append(f"Вс {sunday_hours.get('startStr')} - {sunday_hours.get('endStr')}")
             self.offices.append(
                 {
                     'address': data['address'],
-                    'latlon': [data['latitude'], data['longitude']],
+                    'latlon': [float(data['latitude']), float(data['longitude'])],
                     'name': data['name'],
-                    'phones': [data['phone']],
-                    'working_hours': [weekday, saturday, sunday],
+                    'phones': data['phone'].split(';'),
+                    'working_hours': working_hours,
 
                 }
             )
