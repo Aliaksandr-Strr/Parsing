994507e258ad70417b004ca547a1dc4c86285e9a
Aliaksandr-Strr <58819659+Aliaksandr-Strr@users.noreply.github.com>
2020-10-29 16:04:20 +0300

    Update Pars_mebelshara.py
    Исправлена ошибка в алгориме. Собиралось не все инфо

--- Pars_mebelshara.py
+++ Pars_mebelshara.py
@@ -1,47 +1,38 @@
 import json
-import requests
 from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 
 
 class Parser:
 
     def __init__(self):
-        self.host = 'https://www.mebelshara.ru'
+        self.session = HTMLSession()
         self.url = 'https://www.mebelshara.ru/contacts'
-        self.headers = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36 OPR/70.0.3728.189'
-        }
         self.json = 'address.json'
-        self.contacts = []
+        self.result = []
 
     def get_html(self):
-        try:
-            result = requests.get(self.url, headers=self.headers)
-            self.get_content(result.text)
-        except Exception as ex:
-            print(ex)
-
-    def get_content(self, content):
-        soup = BeautifulSoup(content, 'html.parser')
+        result_html = self.session.get(url=self.url)
+        soup = BeautifulSoup(result_html.text, 'html.parser')
         items = soup.find_all('div', class_='city-item')
         for item in items:
-            self.contacts.append(
-                {
-                    'address': f"{item.find('h4', class_='js-city-name').get_text(strip=True)}," \
-                               f" {item.find('div', class_='shop-list-item')['data-shop-address']}",
-                    'latlon': [f"{item.find('div', class_='shop-list-item')['data-shop-longitude']}," \
-                               f" {item.find('div', class_='shop-list-item')['data-shop-latitude']}"],
-                    'name': f"{item.find('div', class_='shop-list-item')['data-shop-name']}",
-                    'phones': [f"{item.find('div', class_='shop-list-item')['data-shop-phone']}"],
-                    'working_hours': [f"{item.find('div', class_='shop-list-item')['data-shop-mode1']}  " \
-                                      f"{item.find('div', class_='shop-list-item')['data-shop-mode2']}"],
-                })
+            city = item.find('h4', {'class': 'js-city-name'}).get_text(strip=True)
+            shops = item.find_all('div', {'class': 'shop-list-item'})
+            [self.get_content(city, shop) for shop in shops]
+
+    def get_content(self, city, shop):
+        self.result.append({
+            'address': f"{city}, {shop['data-shop-address']}",
+            'latlon': [float(shop['data-shop-latitude']), float(shop['data-shop-longitude'])],
+            'name': shop['data-shop-name'],
+            'phones': shop['data-shop-phone'],
+            'working_hours': [shop['data-shop-mode1'], shop['data-shop-mode2']]
+        })
 
     def write_file(self):
         self.get_html()
         with open(self.json, 'w', encoding='UTF-8') as file:
-            json.dump(self.contacts, file, ensure_ascii=False)
+            json.dump(self.result, file, ensure_ascii=False)
 
 
 if __name__ == '__main__':
